9db7185da3cfdaa718842134113916b440fc6861
mastiaha <mastiaha@mail.ru>
2022-01-10 21:13:21 +0300

    Добавьте файлы проекта.

---
+++ 17 Laba/Bank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Laba
+{
+    class Bank<T>
+    {
+        private T Num { get; set; }
+        private double Balans { get; set; }
+        private string Fio { get; set; }
+
+        internal Bank(T num, double balans, string fio)
+        {
+            Num = num;
+            Balans = balans;
+            Fio = fio;
+        }
+
+        internal string GetInfo()
+        {
+            return $"{Num} {Balans} {Fio}";
+        }
+    }
+}
---
+++ 17 Laba/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета");
+            int numInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс");
+            double balans = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            string fio = Console.ReadLine();
+            Bank<int> vladelec1 = new Bank<int>(numInt, balans, fio);
+            Console.WriteLine(vladelec1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер счета");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            balans = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            fio = Console.ReadLine();
+            Bank<string> vladelec2 = new Bank<string>(numString, balans, fio);
+            Console.WriteLine(vladelec2.GetInfo());
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+
+            Console.ReadKey();
+        }
+    }
+}
